714f92ae5160af9a9e4c8cb2f985b10e6727a8a8
vitorsorpile <vitorsorpile@gmail.com>
2021-06-13 00:42:25 -0300

    Change MoniBot to inherit from commands.bot

--- moniBot.py
+++ moniBot.py
@@ -1,98 +1,93 @@
 import discord
-from discord.ext.commands import command
+from discord.ext import commands
 from decouple import config
-from datetime import date
-from pathlib import Path
+
 
 class User():
-	def __init__(self, discordUser = None, whereToSendAlerts = None):
-		self.discordUser = discordUser
-		self.whereToSendAlerts = whereToSendAlerts
+   def __init__(self, discordUser = None, whereToSendAlerts = None):
+      self.discordUser = discordUser
+      self.whereToSendAlerts = whereToSendAlerts
 
-	def __eq__(self, o: object):
-		if type(o) == User:
-			return self.discordUser.id == o.discordUser.id
-		
-		return self.discordUser.id == o
+   def __eq__(self, o: object):
+      if type(o) == User:
+         return self.discordUser.id == o.discordUser.id
+      
+      return self.discordUser.id == o
 
-	def __hash__(self):
-		return hash(self.discordUser.id)
+   def __hash__(self):
+      return hash(self.discordUser.id)
 
 
-class MoniBot(discord.Client):
-	async def on_ready(self):
-		self.guildsWatched = {}
-		print(f'Logged in as {self.user}')
-		
-	
-	async def on_voice_state_update(self, member, before, after):
-		if after.channel and after.channel.guild.id in self.guildsWatched.keys():
-			guild = after.channel.guild
-			for guild in self.guildsWatched:
-				if member in self.guildsWatched[guild]:
-					return
-		
-			if before.channel != after.channel:
-				print(f'{member} joined {after.channel}')
-				for user in self.guildsWatched[after.channel.guild.id]:
-					await user.whereToSendAlerts.send(f'{member} entrou no canal {after.channel}.')
-	
-	async def on_message(self, message):
-		if(message.author.bot): 
-			return
+class Alerts(commands.Cog):
+   def __init__(self, bot):
+      super().__init__()
+      self.bot = bot
 
-		#COMMAND -> !start
-		if message.content.startswith(('!start', '!iniciar')):
-			guild = message.guild.id
-			user = None
-			# whereToSendAlerts = None
-			if message.raw_channel_mentions:
-				channel = message.guild.get_channel(message.raw_channel_mentions[0])
-				user = User(message.author, channel)
-			else:
-				user = User(message.author, message.author)
+   @commands.command(aliases=['start', 'iniciar'])
+   @commands.has_any_role('Monitor', 'Monitores')
+   async def _start(self, ctx):
+      message = ctx.message
+      guild = message.guild.id
+      user = None
+      if message.raw_channel_mentions:
+         channel = message.guild.get_channel(message.raw_channel_mentions[0])
+         user = User(message.author, channel)
+      else:
+         user = User(message.author, message.author)
 
-			if guild not in self.guildsWatched.keys():
-				self.guildsWatched[guild] = set([user])
-			
-			elif user in self.guildsWatched[guild]:
-				self.guildsWatched[guild].remove(message.author.id)
+      if guild not in self.bot.guildsWatched.keys():
+         self.bot.guildsWatched[guild] = set([user])
+      
+      elif user in self.bot.guildsWatched[guild]:
+         self.bot.guildsWatched[guild].remove(message.author.id)
 
-			self.guildsWatched[guild].add(user)
+      self.bot.guildsWatched[guild].add(user)
 
-			print(f'Now watching {message.guild.name} for {message.author}')
-			await user.whereToSendAlerts.send(f'Observando servidor {message.guild.name} para você!')
-			await message.add_reaction('🤝') 
-			return
+      print(f'Now watching {message.guild.name} for {message.author}')
+      await user.whereToSendAlerts.send(f'Observando servidor {message.guild.name} para você!')
+      await message.add_reaction('🤝') 
 
-		#COMMAND -> !stop
-		if message.content.startswith(('!stop', '!encerrar')):
-			guild = message.guild.id
-			if guild in self.guildsWatched.keys():
-				if message.author.id in self.guildsWatched[guild]:
-					if len(self.guildsWatched[guild]) == 1:
-						self.guildsWatched.pop(guild)
-					else:
-						self.guildsWatched[guild].remove(message.author.id)
-					
-					print(f'Stopped watching {message.guild.name} for {message.author}')
-					await message.author.send(f'Parei de observar o servidor {message.guild.name} para você!')
-					await message.add_reaction('👋')
-			return
 
-		 # matriculas_path = Path().joinpath('matriculas', f'{date.today().strftime("%d-%m-%y")}.txt')
-		# if message.channel.type == discord.ChannelType.text and message.author == self.monitores[1] and message.channel.name == 'matriculas':
-		
-		# 	with matriculas_path.open('a+') as file:
-		# 		file.write(message.content + '\n')
+   @commands.command(aliases=['stop', 'encerrar'])
+   @commands.has_any_role('Monitor', 'Monitores')
+   async def _stop(self, ctx):
+      message = ctx.message
+      guild = message.guild.id
+      if guild in self.bot.guildsWatched.keys():
+         if message.author.id in self.bot.guildsWatched[guild]:
+            if len(self.bot.guildsWatched[guild]) == 1:
+               self.bot.guildsWatched.pop(guild)
+            else:
+               self.bot.guildsWatched[guild].remove(message.author.id)
+            
+            print(f'Stopped watching {message.guild.name} for {message.author}')
+            await message.author.send(f'Parei de observar o servidor {message.guild.name} para você!')
+            await message.add_reaction('👋')
 
-		# 	await message.add_reaction('👌')
+
+class MoniBot(commands.Bot):
+   async def on_ready(self):
+      self.guildsWatched = {}
+      print(f'Logged in as {self.user}')
+      
+   async def on_voice_state_update(self, member, before, after):
+      if after.channel and after.channel.guild.id in self.guildsWatched.keys():
+         guild = after.channel.guild
+         for guild in self.guildsWatched:
+            if member in self.guildsWatched[guild]:
+               return
+      
+         if before.channel != after.channel:
+            print(f'{member} joined {after.channel}')
+            for user in self.guildsWatched[after.channel.guild.id]:
+               await user.whereToSendAlerts.send(f'{member} entrou no canal {after.channel}.')
 
 
 if __name__ == '__main__':
-	intents = discord.Intents.default()  
-	intents.members = True
+   intents = discord.Intents.default()  
+   intents.members = True
 
-	client = MoniBot(intents=intents)
-					
-	client.run(config('BOT_TOKEN'))
+   bot = MoniBot(intents=intents, command_prefix='!', case_insensitive=True)
+   bot.add_cog(Alerts(bot))
+
+   bot.run(config('BOT_TOKEN'))
